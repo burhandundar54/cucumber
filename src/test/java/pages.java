c9a6634149a0e980731bf3bd0ab2d2f19c567ad0
Yeşim Yılmaz <yesimylmz9421@gmail.com>
2022-02-28 23:50:51 +0300

    first

--- src/test/java/pages.java
+++ src/test/java/pages.java
@@ -1,9 +1,6 @@
 public class pages {
     public static void main(String[] args) {
         System.out.println("first");
-        System.out.println("bbb");
-        System.out.println("ccc");
-        System.out.println("aa");
-        System.out.println("xx");
+
     }
 }
