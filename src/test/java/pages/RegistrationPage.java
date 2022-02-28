2d7065d3505235e0171d2730a72c9770b613b478
Tuğba Arıcan <tugbaaricaan@gmail.com>
2022-02-28 23:05:52 +0300

    commit

---
+++ src/test/java/pages/RegistrationPage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class RegistrationPage {
+
+    public RegistrationPage() {
+
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+}
